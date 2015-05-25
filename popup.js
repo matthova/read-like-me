505d24a368fa6b29b6127bbd34125121062eb9ba
Kaitlyn Hova <kaitlynhova@gmail.com>
2015-05-25 16:55:57 -0700

    moving css tag

--- popup.js
+++ popup.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     chrome.tabs.getSelected(null, function(tab){
         var checkpageButton = document.getElementById('checkpage');
         checkpageButton.addEventListener('click', function() {
+          
             chrome.tabs.sendMessage(tab.id,
             {
                 command: "updateText"
