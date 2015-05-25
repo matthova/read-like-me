dcb25a065eba5ca52f7ba68db2733bb0e36e6b78
Kaitlyn Hova <kaitlynhova@gmail.com>
2015-05-25 16:59:14 -0700

    adding jQuery to popup

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     chrome.tabs.getSelected(null, function(tab){
         var checkpageButton = document.getElementById('checkpage');
         checkpageButton.addEventListener('click', function() {
-          
+            $('#checkpage').toggleClass('stop-button');
             chrome.tabs.sendMessage(tab.id,
             {
                 command: "updateText"
